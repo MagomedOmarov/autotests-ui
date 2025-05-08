dd0a3d0978b78b016775682413e230eced50f1a9
magomedomarov <MOmarov@mlg.ru>
2025-05-08 22:26:54 +0300

    задание 6.3 шаг 14

--- test/test_courses.py
+++ test/test_courses.py
@@ -1,8 +1,12 @@
 import json
 import os
+
+import pytest
 from playwright.sync_api import sync_playwright, expect
 
 
+@pytest.mark.courses
+@pytest.mark.regression
 def test_empty_courses_list():
     with sync_playwright() as playwright:
         auth_file = "auth_state.json"
